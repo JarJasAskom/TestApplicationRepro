aed2ee8eb053d22fce4a1b3245f714a38c56aadc
Jarosław Jasiński <jarek.jasinski@gmail.com>
2024-11-25 13:43:44 +0100

    Added EntityFrameworkAdapter

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddDataGridEntityFrameworkAdapter();
 
 
 
