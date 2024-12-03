a21bc6c53aa153ef9da4b3e0661e79faacdcfc04
Jarosław Jasiński <jarek.jasinski@gmail.com>
2024-12-03 12:42:34 +0100

    Test for AutoFit=true and click sort column.

--- Program.cs
+++ Program.cs
@@ -31,6 +31,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var csireContext = services.GetRequiredService<CsireContext>();
+    csireContext.Database.EnsureCreated();
+}
+
+
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
